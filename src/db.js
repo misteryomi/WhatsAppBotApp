1d62ca11ef176b7776b74aba9e7a8955f914fbeb
Yomi Omotoso <31569159+misteryomi@users.noreply.github.com>
2020-08-14 00:41:32 +0100

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,18 +1,9 @@
 // const dotenv = require('dotenv');
 const mysql = require('mysql');
 
-// dotenv.config();
-
-// const { 
-//     DB_HOST: host, 
-//     DB_DATABASE: database,
-//     DB_USER: user, 
-//     DB_PASSWORD: password, 
-// } = process.env;
-//192.254.149.187	
 
 const db = mysql.createConnection({
-    host: '192.254.149.187', user: 'stagepri_whatsap', password: '(hsW$d*-v+7O', database: 'stagepri_whatsapp_bot'
+    host: 'localhost', user: '******', password: '*****', database: '*****'
 });
 
 // console.log({host, user, password, database});
