cb7be417169038b657d8c9281de30db493f5998b
Clicks <clicks@Progressives-MacBook-Pro.local>
2020-10-02 22:54:35 +0100

    Complaints fix

--- src/nodemailer.js
+++ src/nodemailer.js
@@ -17,19 +17,3 @@ var transport = nodemailer.createTransport({
 });
 
 module.exports = transport;
-
-// 'use strict';
-// const nodemailer = require('nodemailer');
-
-// var transport = nodemailer.createTransport({
-//     service:  'Sendgrid',
-//     host: "smtp.sendgrid.net",// "smtp.mailtrap.io",
-//     port: 587, //2525,
-//     auth: {
-//       // api_key: 'pubkey-4776afa332a79a99ac9c75bc7bc8755c',
-//       user: "irsemail", //"95f2109da54f11",
-//       pass: "kingsley12345", //"a73914ab82d7fd"
-//     }
-// });
-
-// module.exports = transport;
