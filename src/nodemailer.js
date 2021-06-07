83f3c27e2c1bfb2a642ddbe361fe46b24e8019cc
Yomi Omotoso <31569159+misteryomi@users.noreply.github.com>
2021-06-07 10:10:08 +0100

    Update nodemailer.js

--- src/nodemailer.js
+++ src/nodemailer.js
@@ -1,44 +1,13 @@
-// 'use strict';
-// const nodemailer = require('nodemailer');
-// const dotenv = require('dotenv');
-
-// dotenv.config();
-
-// const { SENDGRID_USERNAME, SENDGRID_PASSWORD } = process.env;
-
-// var transport = nodemailer.createTransport({
-//     service:  'Sendgrid',
-//     host: "smtp.sendgrid.net",// "smtp.mailtrap.io",
-//     port: 587, //2525,
-//     auth: {
-//       user: SENDGRID_USERNAME, 
-//       pass: SENDGRID_PASSWORD, 
-//     }
-// });
-
-// module.exports = transport;
-
 'use strict';
 const nodemailer = require('nodemailer');
 
-// var transport = nodemailer.createTransport({
-//     service:  'Sendgrid',
-//     host: "smtp.sendgrid.net",// "smtp.mailtrap.io",
-//     port: 587, //2525,
-//     auth: {
-//       // api_key: 'pubkey-4776afa332a79a99ac9c75bc7bc8755c',
-//       user: "irsemail", //"95f2109da54f11",
-//       pass: "kingsley12345", //"a73914ab82d7fd"
-//     }
-// });
-
 var transport = nodemailer.createTransport({
-  host: "smtp-mail.outlook.com", // hostname
+  host: process.env.MAIL_HOST, // hostname
   secureConnection: false, // TLS requires secureConnection to be false
-  port: 587, // port for secure SMTP
+  port: process.env.MAIL_PORT, // port for secure SMTP
   auth: {
-    user: "noreply@primeramfbank.com", //"95f2109da54f11",
-    pass: "default@12345", //"a73914ab82d7fd"
+    user:process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   tls: {
       ciphers:'SSLv3'
